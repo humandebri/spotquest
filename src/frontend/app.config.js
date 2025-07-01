b5547d8fc31cf20a791c46cba9c76c73ac9d506b
hude <qwertyuiop123456@gmail.com>
2025-07-01 19:01:46 +0900

    Bump iOS build number to 13 for crash fix release

--- src/frontend/app.config.js
+++ src/frontend/app.config.js
@@ -18,7 +18,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.spotquest.app",
-      buildNumber: "12",
+      buildNumber: "13",
       config: {
         googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
       },
